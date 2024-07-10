d556e8fb4db06bdf06e30746f52b38f332529f79
yicongyuan <waiwaixi914@gmail.com>
2024-07-10 21:27:32 +0800

    锁定敌人并根据方向发射

--- bullets/BulletShooter.gd
+++ bullets/BulletShooter.gd
@@ -10,6 +10,8 @@ var shoot_time=2
 var bullet_speed=200
 var bullet_damage=1
 
+var target_enemy:Enemy=null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -22,10 +24,26 @@ func _process(delta):
 
 
 func _on_shoot_timer_timeout():
-	var new_bullet = bullet.instantiate()
-	new_bullet.speed = bullet_speed
-	new_bullet.damage = bullet_damage
-	new_bullet.position = shoot_point.global_position
-	
-	new_bullet.dir = Vector2(1,0)
-	get_tree().root.add_child(new_bullet)
+	if target_enemy!=null:
+		
+		var new_bullet = bullet.instantiate()
+		new_bullet.speed = bullet_speed
+		new_bullet.damage = bullet_damage
+		new_bullet.position = shoot_point.global_position
+		
+		
+		
+		new_bullet.dir = (target_enemy.global_position- shoot_point.global_position).normalized()
+		print("发射方向:%s"%new_bullet.dir)
+		get_tree().root.add_child(new_bullet)
+
+
+func _on_body_entered(body:Enemy):
+	if target_enemy==null:
+		target_enemy=body
+		print("锁定新的敌人")
+
+func _on_body_exited(body:Enemy):
+	if target_enemy==body:
+		target_enemy=null
+		print("之前的敌人离开")
