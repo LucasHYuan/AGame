41c6b19f6a61894dd317b48fefd082c50f0858ce
yicongyuan <waiwaixi914@gmail.com>
2024-07-21 21:14:48 +0800

    修复骷髅隐身问题：tilemap不使用y-sort模式，而用z-index显示在所有东西下方

--- bullets/Bullet.gd
+++ bullets/Bullet.gd
@@ -1,21 +1,21 @@
-extends CharacterBody2D
+extends Node2D
 
 @onready var hitbox: Hitbox = $Hitbox
 
 var dir=Vector2.ZERO
-var speed=500
+var speed=1
 var atk=1
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_as_top_level(true)
 	hitbox.hit.connect(_on_hitbox_hit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity=dir*speed
-	move_and_slide()
+	position += (dir * speed) * delta
 
 func _on_hitbox_hit(hurtbox:Hurtbox) -> void:
 	queue_free()
