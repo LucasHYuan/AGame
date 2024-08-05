236921071911569ca0f8bfdae72692336c483626
yicongyuan <waiwaixi914@gmail.com>
2024-08-05 17:48:24 +0800

    添加GM指令按钮

---
+++ scripts/GMCommand.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func playerLevelUp():
+	print("GM指令：升级！")
+	
+func nextTime():
+	print("GM指令：昼夜交替！")
