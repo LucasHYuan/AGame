52e9cf0c0eefacbc7c300377306d2582780b524f
yicongyuan <waiwaixi914@gmail.com>
2024-08-15 23:50:24 +0800

    让农场每天给钱

--- buildings/farm_basic.gd
+++ buildings/farm_basic.gd
@@ -1,6 +1,13 @@
 extends Buildings
 
+@export var coin_earn_day: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
+
+func _on_day_function() -> void:
+	# 玩家金币+1
+	var player = GlobalObjects.GetObject("player")
+	if player:
+		player.stats.coin += coin_earn_day
