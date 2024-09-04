99989a0c1e26396d5712070e5b519d12655f0187
YYC <waiwaixi914@gmail.com>
2024-09-04 16:41:12 +0800

    重构昼夜表现控制器

--- cycle_controller.gd
+++ cycle_controller.gd
@@ -4,6 +4,17 @@ extends Node2D
 signal day
 signal night
 
+
+## 世界在白天黑夜中不断循环
+## 白天是休息阶段，也会刷怪，但不构成实质威胁
+## 夜晚是怪物进攻阶段，压力激增
+## cycle_controller 负责白天黑夜切换的表现
+## 1 白天光亮、夜晚黑暗，并且颜色随时间逐渐变化
+## 2 在白天、黑夜开始时，出现暂时的文字，告诉玩家敌人要进攻了or可以喘息
+## 3 阶段内，显示时间的倒计时
+## 4 由cycle_controller控制刷怪器进行
+
+
 @onready var day_night_modulate: CanvasModulate = $DayNightModulate
 @onready var state_timer: Timer = $StateTimer
 @onready var one_second_timer: Timer = $OneSecondTimer
@@ -11,89 +22,112 @@ signal night
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var countdown: Label = $CanvasLayer/Countdown
 
-@export var night_time: float = 60.0
-@export var day_time: float = 60.0
-@export var transition_time : float = 12
-@export var colors: Array = [
+@export var day_colors: Array = [
+	Color(1, 1, 1), # Mid day
+	Color(0.843, 0.798, 0.688), # Afternoon
+	Color(0.554, 0.589, 0.709), # Dusk
+]
+@export var night_colors: Array = [
 	Color(0, 0.431, 1),
-	Color(0.161,0.573,0.769),
+	Color(0.161, 0.573, 0.769),
 	Color(0.224, 0.243, 0.333),
 	Color(0.867, 0.741, 0.525),
-	Color(1,1,1), #Mid day
-	Color(0.843, 0.798, 0.688), #Afternoon
-	Color(0.554, 0.589, 0.709), #Dusk
 ]
-@export var current_state: State = State.NIGHT
-@export var start_state: State = State.NIGHT
-enum State{
-	DAY,
-	NIGHT,
+@export var start_state: State = State.DAY
+@onready var current_state: State = start_state
+
+enum State {
+	DAY = 0,
+	NIGHT = 1,
 }
 
+var colors: Array = []
 var color_index: int = 0
+
+## 从白天开始，每晚时间不同
+var day_index = 0
+var day_time = [20, 30, 30, 30, 30]
+var night_time = [30, 30, 30, 30, 30]
+var times = []
+var is_day: bool:
+	get:
+		return current_state == State.DAY
 
 func _ready() -> void:
 	# 监听GM指令
-	GlobalSignal.add_listener("gmChangeTime", self, "_on_timer_timeout")
+	GlobalSignal.add_listener("gmChangeTime", self, "_change_state")
 
 	# 注册全局指令
 	GlobalSignal.add_emitter("day", self)
 	GlobalSignal.add_emitter("night", self)
 
-	day_night_modulate.color = colors[color_index]
-	state_timer.start(night_time)
-	state_timer.timeout.connect(_on_timer_timeout)
-	one_second_timer.timeout.connect(_one_second_pass)
-	transition_timer.timeout.connect(_transition_time_out)
-	transition_timer.start(0.0)
-	one_second_timer.start(1.0)
-	countdown.text = str(int(state_timer.time_left))
+	state_timer.timeout.connect(_change_state)
+	one_second_timer.timeout.connect(_count_down)
+	transition_timer.timeout.connect(_transition_next_color)
 
-func _one_second_pass() -> void:
-	var remaining_time: int = int(state_timer.time_left)
-	if remaining_time > 0:
-		remaining_time -= 1
-		countdown.text = str(remaining_time)
-		
-	
-func _transition_time_out() -> void:
-	color_index = (color_index + 1) % colors.size()
-	transition_to_color(colors[color_index])
-	transition_timer.start(transition_time)
+	_start_by_state() # 根据当前状态启动
+
+
+func _count_down() -> void:
+	var remaining_time: int = round(state_timer.time_left) # 截断小数强制转换
+	_set_count_down_text(remaining_time)
+
+func _set_count_down_text(t: int) -> void:
+	countdown.text = str(t)
 	
 # 日夜交替
-func _on_timer_timeout() -> void:
-	next_state()
-	color_index = (color_index + 1) % colors.size()
-	transition_to_color(colors[color_index])
-	transition_timer.start(transition_time)
+func _change_state() -> void:
+	_set_next_state()
+	_start_by_state()
 
-func next_state() -> void:
+# 设置昼夜状态
+func _set_next_state() -> void:
 	if current_state == State.DAY:
-		start_night()
+		current_state = State.NIGHT
 	else:
-		start_day()
-
-func start_day() -> void:
-	current_state = State.DAY
-	state_timer.start(day_time)
-	day.emit()
-
-func start_night() -> void:
-	current_state = State.NIGHT
-	state_timer.start(night_time)
-	night.emit()
+		current_state = State.DAY
+		day_index += 1
 	
+# 根据当前状态开始计时
+func _start_by_state() -> void:
+	if current_state == State.DAY:
+		times = day_time
+		day.emit()
+	else:
+		times = night_time
+		night.emit()
+	var _time = times[0]
+	state_timer.start(_time)
+	_start_transition()
+	_set_count_down_text(_time)
 
-func transition_to_color(target_color: Color) -> void:
+	# 开始数字倒计时
+	one_second_timer.start(1.0)
+
+
+func _start_transition() -> void:
+	colors = day_colors if current_state == State.DAY else night_colors
+	color_index = 0
+	_transition_next_color()
+
+# 整段渐变由多个颜色组成，在此切换
+func _transition_next_color() -> void:
+	var duration = times[day_index] / float(colors.size())
+	_transition_to_color(colors[color_index], duration)
+	color_index = (color_index + 1) % colors.size()
+	if color_index > 0:
+		transition_timer.start(duration)
+	else:
+		transition_timer.stop()
+
+
+# 在一段时间内切换到目标颜色
+func _transition_to_color(target_color: Color, duration: float) -> void:
+	print(target_color, "  ", duration)
 	var anim = animation_player.get_animation("transition")
 	if anim:
-		anim.length = transition_time
-		var track_index = 0
-		anim.track_insert_key(0, 0, day_night_modulate.color)
-		anim.track_insert_key(0, transition_time, target_color)
+		anim.length = duration
+		anim.track_insert_key(0, 0, day_night_modulate.color) # 设置当前颜色
+		anim.track_insert_key(0, duration, target_color) # 设置目标颜色
 		animation_player.stop()
 		animation_player.play("transition")
-
-
-	
